d0b836f73ff9f93624888e4cf68d1b3178ebd8e0
CamNoName <crgrohler@gmail.com>
2024-03-15 23:33:36 -0400

    Added google button.
    Placed sign in button and placed onClicklistener

--- app/src/main/java/com/example/spotifywrappedproject2/Login.java
+++ app/src/main/java/com/example/spotifywrappedproject2/Login.java
@@ -3,13 +3,24 @@ package com.example.spotifywrappedproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    private Button googleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        googleButton = (Button) findViewById(R.id.google_sign_in);
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
