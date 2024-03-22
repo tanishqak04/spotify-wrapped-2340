d02e4032ee4bc49cfccdf406d63ea0e7b6f8a08d
CamNoName <crgrohler@gmail.com>
2024-03-21 20:10:17 -0400

    Revert "Added google button."
    This reverts commit 9aa4dd394228a9759c78d36ac7c651ed1b2cbf22.

--- app/src/main/java/com/example/spotifywrappedproject2/Login.java
+++ app/src/main/java/com/example/spotifywrappedproject2/Login.java
@@ -3,24 +3,12 @@ package com.example.spotifywrappedproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
-    private Button googleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        googleButton = (Button) findViewById(R.id.google_sign_in);
-        googleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                
-            }
-        });
     }
 }
