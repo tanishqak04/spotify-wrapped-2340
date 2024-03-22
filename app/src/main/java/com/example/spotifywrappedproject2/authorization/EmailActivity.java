4de58d5be63705540c597ba1283d442c4c59140a
CamNoName <crgrohler@gmail.com>
2024-03-21 21:22:08 -0400

    activity and xml
    Added Email Activity and xml files to git

---
+++ app/src/main/java/com/example/spotifywrappedproject2/authorization/EmailActivity.java
@@ -0,0 +1,16 @@
+package com.example.spotifywrappedproject2.authorization;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spotifywrappedproject2.R;
+
+public class EmailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_auth);
+    }
+
+}
