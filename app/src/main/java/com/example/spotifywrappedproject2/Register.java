d28f4fd00a9dc3baacb782b5eec34265ba6c1dca
CamNoName <crgrohler@gmail.com>
2024-03-23 15:28:20 -0500

    Registration Firebase
    Implemented user creation using firebase for registering accounts.

--- app/src/main/java/com/example/spotifywrappedproject2/Register.java
+++ app/src/main/java/com/example/spotifywrappedproject2/Register.java
@@ -1,14 +1,56 @@
 package com.example.spotifywrappedproject2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+        /*
+        * Code to register users to Firebase once email and password
+        * are obtained.
+        *
+        * Needs Email and Password as Strings obtained from user input
+        *
+        *  */
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // User is registered successfully
+                            FirebaseUser user = mAuth.getCurrentUser(); //Optionally get ref to user
+
+                            //******Take user to next activity and/or display toast confirming******
+
+
+
+                        } else {
+                            // If sign in fails, displays a toast
+                            Toast.makeText(Register.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
