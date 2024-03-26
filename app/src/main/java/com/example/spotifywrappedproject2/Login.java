fd39eb64a22c5966bd1ec47e5451f2017379812c
CamNoName <crgrohler@gmail.com>
2024-03-26 13:26:18 -0400

    Login Code

--- app/src/main/java/com/example/spotifywrappedproject2/Login.java
+++ app/src/main/java/com/example/spotifywrappedproject2/Login.java
@@ -1,15 +1,64 @@
 package com.example.spotifywrappedproject2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
+    FirebaseAuth mAuth;
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null).
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //add implementation for user if they are already logged in.
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+
+        /*
+         * Code to login users to Firebase once email and password
+         * are obtained
+         *
+         * Needs Email and Password as Strings obtained from user input
+         *
+         *  */
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, move to next activity or show user login success
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+
+
+                        } else {
+                            // If sign in fails, display a toast
+                            Toast.makeText(Login.this, "Login failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
