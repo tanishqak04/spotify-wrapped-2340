7be434559d501e7b044212594c4b884bae1cefa4
YeongbinKim2000 <yeongbin0811@gmail.com>
2024-04-01 22:59:04 -0400

    Spinner is on main page

--- app/src/main/java/com/example/spotifywrappedproject2/PastWrappedScreen.java
+++ app/src/main/java/com/example/spotifywrappedproject2/PastWrappedScreen.java
@@ -1,14 +1,13 @@
 package com.example.spotifywrappedproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class PastWrappedScreen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_wrapped_screen);
+
     }
 }
