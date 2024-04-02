322b72b715e9c93188289b5e023b035da334a344
jackhughes5 <jackhhughes5@gmail.com>
2024-04-02 13:34:27 -0400

    backButtonStart

--- app/src/main/java/com/example/spotifywrappedproject2/PastWrappedScreen.java
+++ app/src/main/java/com/example/spotifywrappedproject2/PastWrappedScreen.java
@@ -3,6 +3,8 @@ package com.example.spotifywrappedproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PastWrappedScreen extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class PastWrappedScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_wrapped_screen);
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
