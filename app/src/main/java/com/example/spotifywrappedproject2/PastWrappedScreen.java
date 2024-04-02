0dac25269b705fdbfe31277cc98997c6afe6bf35
YeongbinKim2000 <yeongbin0811@gmail.com>
2024-04-02 14:52:25 -0400

    revised spinner

--- app/src/main/java/com/example/spotifywrappedproject2/PastWrappedScreen.java
+++ app/src/main/java/com/example/spotifywrappedproject2/PastWrappedScreen.java
@@ -1,7 +1,12 @@
 package com.example.spotifywrappedproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.text.Html;
+import android.view.MenuItem;
+import android.view.View;
 
 public class PastWrappedScreen extends AppCompatActivity {
     @Override
@@ -9,5 +14,28 @@ public class PastWrappedScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_wrapped_screen);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Show the Up button in the action bar.
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Past Wrapped");
+        }
     }
+
+    // This method handles the action bar item clicks
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this activity,
+            // the Up button is shown. For more details, see the Navigation pattern on Android Design:
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            finish(); // Will close the current activity and go back to UserStoryMainPage
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
