b62567a2170af6439ed6b020ac4aa97cfe0bffaa
Jseal7 <157525096+Jseal7@users.noreply.github.com>
2024-04-08 17:30:25 -0400

    Easter Wrap Implemented

---
+++ app/src/main/java/com/example/spotifywrappedproject2/easter_wrap.java
@@ -0,0 +1,14 @@
+package com.example.spotifywrappedproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class easter_wrap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easter_wrap);
+    }
+}
