f55bf2bb82151a27fc7ce619818072f5fc4c5180
CamNoName <crgrohler@gmail.com>
2024-04-10 23:37:45 -0400

    Fixed Wrapped

--- app/src/main/java/com/example/spotifywrappedproject2/Wrapped.java
+++ app/src/main/java/com/example/spotifywrappedproject2/Wrapped.java
@@ -3,94 +3,32 @@ package com.example.spotifywrappedproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
+import android.os.Environment;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class Wrapped extends AppCompatActivity {
-    private String accessToken;
-
-    private int[] imageViewIds = {R.id.albumCover1, R.id.albumCover2, R.id.albumCover3, R.id.albumCover4, R.id.albumCover5};
-    private int[] textViewIds = {R.id.songTitle1, R.id.songTitle2, R.id.songTitle3, R.id.songTitle4, R.id.songTitle5};
-
+    private RelativeLayout relativeLayout;
     private boolean userInteracted = false;
     private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wrapped);
-
-        accessToken = getIntent().getStringExtra("accessToken");
-        API api = new API(accessToken);
-
-
-        api.getTopTracks(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.e("Wrapped", "Network error: ", e);
-                runOnUiThread(() -> Toast.makeText(Wrapped.this, "Network error: " + e.getMessage(), Toast.LENGTH_SHORT).show());
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                final String responseBody = response.body().string();
-                Log.d("Wrapped", "API Response: " + responseBody);
-                if (!response.isSuccessful()) {
-                    runOnUiThread(() -> Toast.makeText(Wrapped.this, "Error fetching top tracks: " + response.message(), Toast.LENGTH_SHORT).show());
-                    return;
-                }
-
-                try {
-                    JSONObject jsonObject = new JSONObject(responseBody);
-                    final JSONArray tracks = jsonObject.getJSONArray("items");
-
-                    runOnUiThread(() -> {
-                        try {
-                            for (int i = 0; i < tracks.length() && i < imageViewIds.length; i++) {
-                                JSONObject track = tracks.getJSONObject(i);
-                                JSONObject album = track.getJSONObject("album");
-                                JSONArray images = album.getJSONArray("images");
-                                String imageUrl = images.getJSONObject(0).getString("url");
-                                String trackName = track.getString("name");
-
-                                ImageView imageView = findViewById(imageViewIds[i]);
-                                TextView textView = findViewById(textViewIds[i]);
-
-                                // Use an image loading library like Glide to load the image
-                                Glide.with(Wrapped.this).load(imageUrl).into(imageView);
-
-                                textView.setText(trackName);
-                            }
-                        } catch (JSONException e) {
-                            Log.e("Wrapped", "JSON parsing error: " + e.getMessage());
-                        }
-                    });
-                } catch (JSONException e) {
-                    Log.e("Wrapped", "Error parsing top tracks response: " + e.getMessage());
-                }
-            }
-        });
 
         //Initialize the spinner
         spinner = findViewById(R.id.spinner);
@@ -123,6 +61,57 @@ public class Wrapped extends AppCompatActivity {
             @Override
             public void onNothingSelected(AdapterView<?> parent) {}
         });
+
+        // export
+
+        relativeLayout = findViewById(R.id.myLayout);
+        Button exportButton = (Button) findViewById(R.id.exportButton);
+
+        exportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("heya");
+                saveImage();
+            }
+        });
+    }
+
+    private void saveImage() {
+        relativeLayout.setDrawingCacheEnabled(true);
+        relativeLayout.buildDrawingCache();
+        relativeLayout.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        Bitmap bitmap = relativeLayout.getDrawingCache();
+
+        save(bitmap);
+
+    }
+
+    private void save(Bitmap bitmap) {
+
+        String root = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File file = new File(root + "/Download");
+        String fileName = "img" + Calendar.getInstance().get(Calendar.MILLISECOND) + ".jpg";
+        File myFile = new File(file, fileName);
+
+        if (myFile.exists()) {
+            myFile.delete();
+        }
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(myFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+            relativeLayout.setDrawingCacheEnabled(false);
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            Toast.makeText(this, "Error : " + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
     @Override
