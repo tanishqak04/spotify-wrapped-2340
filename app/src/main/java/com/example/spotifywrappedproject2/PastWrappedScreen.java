963323ef73f211fb93a15e63e0a6cb32cb778cfd
jackhughes5 <jackhhughes5@gmail.com>
2024-04-10 22:04:00 -0400

    UI Setup for PW

--- app/src/main/java/com/example/spotifywrappedproject2/PastWrappedScreen.java
+++ app/src/main/java/com/example/spotifywrappedproject2/PastWrappedScreen.java
@@ -1,7 +1,9 @@
 package com.example.spotifywrappedproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,56 @@ public class PastWrappedScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        CardView cardViewWrap23 = (CardView) findViewById(R.id.wrap23);
+        cardViewWrap23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PastWrappedScreen.this, wrapped23.class);
+                startActivity(intent);
+            }
+        });
+
+        CardView cardViewWrap22 = (CardView) findViewById(R.id.wrap22);
+        cardViewWrap22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PastWrappedScreen.this, wrapped22.class);
+                startActivity(intent);
+            }
+        });
+        CardView cardViewWrap21 = (CardView) findViewById(R.id.wrap21);
+        cardViewWrap21.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PastWrappedScreen.this, wrapped21.class);
+                startActivity(intent);
+            }
+        });
+        CardView cardViewWrap20 = (CardView) findViewById(R.id.wrap20);
+        cardViewWrap20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PastWrappedScreen.this, wrapped20.class);
+                startActivity(intent);
+            }
+        });
+        CardView cardViewWrap19 = (CardView) findViewById(R.id.wrap19);
+        cardViewWrap19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PastWrappedScreen.this, wrapped19.class);
+                startActivity(intent);
+            }
+        });
+        CardView cardViewWrap18 = (CardView) findViewById(R.id.wrap18);
+        cardViewWrap18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PastWrappedScreen.this, wrapped18.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/spotifywrappedproject2/wrapped21.java
@@ -0,0 +1,23 @@
+package com.example.spotifywrappedproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class wrapped21 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wrapped21);
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
