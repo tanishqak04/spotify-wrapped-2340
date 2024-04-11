9898e041077a03dba17298a24017e3444eab6696
tanishqak04 <109610019+tanishqak04@users.noreply.github.com>
2024-04-10 23:18:34 -0400

    update acc navigation

--- app/src/main/java/com/example/spotifywrappedproject2/Settings.java
+++ app/src/main/java/com/example/spotifywrappedproject2/Settings.java
@@ -2,7 +2,11 @@ package com.example.spotifywrappedproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Button updateAcc = findViewById(R.id.updateacc);
+        updateAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = null;
+                myIntent = new Intent(Settings.this, UpdateAcc.class);
+                if (myIntent != null) {
+                    startActivity(myIntent);
+                }
+            }
+
+        });
     }
+
+
 }
