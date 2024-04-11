969db21e8a716b52601e68e33aacbf097907f99b
tanishqak04 <109610019+tanishqak04@users.noreply.github.com>
2024-04-10 23:18:34 -0400

    update acc navigation

--- app/src/main/java/com/example/spotifywrappedproject2/UpdateAcc.java
+++ app/src/main/java/com/example/spotifywrappedproject2/UpdateAcc.java
@@ -1,5 +1,7 @@
 package com.example.spotifywrappedproject2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,4 +13,21 @@ public class UpdateAcc extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_acc);
     }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        new  AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to delete your account?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
+
+    }
+
 }
