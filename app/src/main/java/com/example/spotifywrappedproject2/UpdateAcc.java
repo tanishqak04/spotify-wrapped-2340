bd3191f0ea82f16ba00f4848088effebadb8f80e
CamNoName <crgrohler@gmail.com>
2024-04-11 12:12:20 -0400

    Edit Details
    edit login components

--- app/src/main/java/com/example/spotifywrappedproject2/UpdateAcc.java
+++ app/src/main/java/com/example/spotifywrappedproject2/UpdateAcc.java
@@ -5,21 +5,70 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class UpdateAcc extends AppCompatActivity {
+    EditText editTextEmail;
+    EditText editTextPassword;
+    String email, password;
+    Button submitButton;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_acc);
+
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        submitButton = findViewById(R.id.updateButton);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                currentUser.updateEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(UpdateAcc.this, "Email Updated",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                                currentUser.updatePassword(password)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(UpdateAcc.this, "Password Updated",
+                                                            Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+
+                            }
+                        });
+
+            }
+
+
+        });
     }
     @Override
     public void onBackPressed() {
@@ -30,20 +79,7 @@ public class UpdateAcc extends AppCompatActivity {
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-                        user.delete()
-                                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if (task.isSuccessful()) {
-                                            Intent intent = new Intent(UpdateAcc.this, Register.class);
-                                            startActivity(intent);
-                                        }
-                                    }
-                                });
-
-                        ;
+                        finish();
                     }
                 })
                 .setNegativeButton("No", null)
