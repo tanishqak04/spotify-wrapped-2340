bad322a7087e9e1473afb3831a3ff4b9a99737f0
Jseal7 <157525096+Jseal7@users.noreply.github.com>
2024-04-16 14:55:31 -0400

    Back Buttons to Holiday

--- app/src/main/java/com/example/spotifywrappedproject2/ChristmasWrap.java
+++ app/src/main/java/com/example/spotifywrappedproject2/ChristmasWrap.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,6 +35,16 @@ public class ChristmasWrap extends AppCompatActivity {
 
         accessToken = getIntent().getStringExtra("accessToken");
         API api = new API(accessToken);
+
+        Button backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
 
         api.getTopTracks10(new Callback() {
