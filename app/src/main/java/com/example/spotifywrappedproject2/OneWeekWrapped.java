df06035ff8f1fae7a84015056c4cccdc3576ee50
jackhughes5 <jackhhughes5@gmail.com>
2024-04-16 13:21:26 -0400

    oneWeek

--- app/src/main/java/com/example/spotifywrappedproject2/OneWeekWrapped.java
+++ app/src/main/java/com/example/spotifywrappedproject2/OneWeekWrapped.java
@@ -2,12 +2,39 @@ package com.example.spotifywrappedproject2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class OneWeekWrapped extends AppCompatActivity {
-
+    private API api;
+    private String accessToken;
+    private int[] imageViewIds = {R.id.albumCover1, R.id.albumCover2, R.id.albumCover3, R.id.albumCover4, R.id.albumCover5};
+    private int[] textViewIds = {R.id.songTitle1, R.id.songTitle2, R.id.songTitle3, R.id.songTitle4, R.id.songTitle5};
+    private RelativeLayout relativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +46,104 @@ public class OneWeekWrapped extends AppCompatActivity {
                 finish();
             }
         });
+        Intent intent = getIntent();
+        accessToken = intent.getStringExtra("accessToken");
+
+        if (accessToken == null || accessToken.isEmpty()) {
+            Toast.makeText(this, "Access token is missing", Toast.LENGTH_LONG).show();
+            finish(); // End this activity due to lack of token
+            return;
+        }
+        api = new API(accessToken); // Correctly passing the accessToken here
+
+
+
+        api.shortTerm(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("Wrapped", "Network error: ", e);
+                runOnUiThread(() -> Toast.makeText(OneWeekWrapped.this, "Network error: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseBody = response.body().string();
+                Log.d("Wrapped", "API Response: " + responseBody);
+                if (!response.isSuccessful()) {
+                    runOnUiThread(() -> Toast.makeText(OneWeekWrapped.this, "Error fetching top tracks: " + response.message(), Toast.LENGTH_SHORT).show());
+                    return;
+                }
+
+                try {
+                    JSONObject jsonObject = new JSONObject(responseBody);
+                    final JSONArray tracks = jsonObject.getJSONArray("items");
+
+                    runOnUiThread(() -> {
+                        try {
+                            for (int i = 0; i < tracks.length() && i < imageViewIds.length; i++) {
+                                JSONObject track = tracks.getJSONObject(i);
+                                JSONObject album = track.getJSONObject("album");
+                                JSONArray images = album.getJSONArray("images");
+                                String imageUrl = images.getJSONObject(0).getString("url");
+                                String trackName = track.getString("name");
+
+                                ImageView imageView = findViewById(imageViewIds[i]);
+                                TextView textView = findViewById(textViewIds[i]);
+
+                                // Use an image loading library like Glide to load the image
+                                Glide.with(OneWeekWrapped.this).load(imageUrl).into(imageView);
+
+                                textView.setText(trackName);
+                            }
+                        } catch (JSONException e) {
+                            Log.e("Wrapped", "JSON parsing error: " + e.getMessage());
+                        }
+                    });
+                } catch (JSONException e) {
+                    Log.e("Wrapped", "Error parsing top tracks response: " + e.getMessage());
+                }
+            }
+        });
+
+
+    }
+    private void saveImage() {
+        relativeLayout.setDrawingCacheEnabled(true);
+        relativeLayout.buildDrawingCache();
+        relativeLayout.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        Bitmap bitmap = relativeLayout.getDrawingCache();
+
+        save(bitmap);
+
+    }
+
+    private void save(Bitmap bitmap) {
+
+        String root = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File file = new File(root + "/Download");
+        String fileName = "img" + Calendar.getInstance().get(Calendar.MILLISECOND) + ".jpg";
+        File myFile = new File(file, fileName);
+
+        if (myFile.exists()) {
+            myFile.delete();
+        }
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(myFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+            relativeLayout.setDrawingCacheEnabled(false);
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            Toast.makeText(this, "Error : " + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
+
+
