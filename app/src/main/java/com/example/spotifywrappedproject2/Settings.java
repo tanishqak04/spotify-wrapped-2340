506713af50abfdb2932cb83129381c86a5b7d587
tanishqak04 <109610019+tanishqak04@users.noreply.github.com>
2024-04-18 12:40:07 -0400

    dark mode function

--- app/src/main/java/com/example/spotifywrappedproject2/Settings.java
+++ app/src/main/java/com/example/spotifywrappedproject2/Settings.java
@@ -3,7 +3,9 @@ package com.example.spotifywrappedproject2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,8 +13,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+
 
 public class Settings extends AppCompatActivity {
+
+    Switch toggle;
+    boolean nightTog;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     Class<?> className;
     @Override
@@ -22,6 +34,31 @@ public class Settings extends AppCompatActivity {
         Button updateAcc = findViewById(R.id.updateacc);
         ImageButton back = findViewById(R.id.backArrow);
 
+        getSupportActionBar().hide();
+
+        toggle = findViewById(R.id.toggle);
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightTog = sharedPreferences.getBoolean("night", false);
+
+        if (nightTog) {
+            toggle.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        toggle.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (nightTog) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", false);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("night", true);
+                }
+                editor.apply();
+            }
+        });
+
         Intent intent = getIntent();
         if (intent != null) {
             try {
@@ -30,7 +67,8 @@ public class Settings extends AppCompatActivity {
                 throw new RuntimeException(e);
             }
         }
-        updateAcc.setOnClickListener(new View.OnClickListener() {
+        updateAcc.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
                 Intent myIntent = null;
@@ -50,6 +88,5 @@ public class Settings extends AppCompatActivity {
             }
         });
     }
-
 
 }
